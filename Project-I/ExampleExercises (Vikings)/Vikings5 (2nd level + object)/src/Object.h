acfc5c43df76040e4e0bfd4aa9bc0729b15e9da5
Keli11 <kelivima2005@gmail.com>
2024-04-12 12:25:28 +0200

    Vikings5 (ChangeLevel + Objects)

---
+++ Project-I/ExampleExercises (Vikings)/Vikings5 (2nd level + object)/src/Object.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Entity.h"
+
+#define OBJECT_PHYSICAL_SIZE	14
+#define OBJECT_FRAME_SIZE		16
+
+#define POINTS_APPLE		10
+#define POINTS_CHILI		20
+#define POINTS_KEY_RED		40
+#define POINTS_KEY_YELLOW	50
+
+enum class ObjectType { APPLE, CHILI, KEY_RED, KEY_YELLOW};
+
+class Object : public Entity
+{
+public:
+	Object(const Point& p, ObjectType t);
+	~Object();
+
+	void DrawDebug(const Color& col) const;
+	int Points() const;
+
+private:
+	ObjectType type;
+};
+
